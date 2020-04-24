859062dcf2d513271481cff2f4c04478257059db
Robert Fernandez <robert.fernandez@mymona.edu.jm>
2020-04-24 12:57:58 -0500

    Corrected Fetch According to Lab6 Requirements

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -72,7 +72,7 @@ const NewsList = Vue.component('news-list', {
     created: function() {
         let self = this;
         
-        fetch('https://newsapi.org/v2/top-headlines?q=coronavirus&language=en&apiKey=<your_API_key>')
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<your_API_key>')
         .then(function(response) {
             return response.json();
         })
